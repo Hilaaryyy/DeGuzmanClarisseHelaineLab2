42c48defc53f989956a956c1f2e8f91b774bd823
Hilary Casitas <hilary.casitas@gmail.com>
2018-09-08 21:34:09 +0800

    commit-1

---
+++ AndroidStudioProjects/DeGuzmanClarisseHelaineLab2/app/src/main/java/com/deguzman/clarissehelaine/deguzmanclarissehelainelab2/Constraint.java
@@ -0,0 +1,13 @@
+package com.deguzman.clarissehelaine.deguzmanclarissehelainelab2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Constraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout);
+    }
+}
